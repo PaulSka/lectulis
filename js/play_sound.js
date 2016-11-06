ae87f71b5038069c0f8c2b4ca22dcf9aec15b22c
Mathieu LYSAKOWSKI <lysakowskimg@gmail.com>
2016-11-06 14:49:02 +0100

    Add function to remove accent for only one character

--- js/play_sound.js
+++ js/play_sound.js
@@ -24,15 +24,40 @@ function tts_all() {
 		//Verification
 		if (value == "." || value == "!" || value == "," || value == "?") {
 			$("#show_tts").append('<a class="btn btn-primary btn-large" href="#" role="button">' + value + '</a>');
-		} else if (value.indexOf("<niv2>") >=0) {
-			$("#show_tts").append('<a class="btn btn-niv2 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
-			value.replace("<niv2>", "").replace("</niv2>", "") + "'" + ')">' + value.replace("<niv2>", "").replace("</niv2>", "") + '</a>');
-		} else if (value.indexOf("<niv3>") >=0) {
-			$("#show_tts").append('<a class="btn btn-niv3 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
-			value.replace("<niv3>", "").replace("</niv3>", "") + "'" + ')">' + value.replace("<niv3>", "").replace("</niv3>", "") + '</a>');
-		} else {
+		} else if (value.length == 1) {
 			$("#show_tts").append('<a class="btn btn-niv1 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
-			value + "'" + ')">' + value + '</a>');
+				remove_accent(value) + "'" + ')">' + value + '</a>');
+		} else {
+			if (value.indexOf("<niv2>") >=0) {
+				$("#show_tts").append('<a class="btn btn-niv2 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
+				value.replace("<niv2>", "").replace("</niv2>", "") + "'" + ')">' + value.replace("<niv2>", "").replace("</niv2>", "") + '</a>');
+			} else if (value.indexOf("<niv3>") >=0) {
+				$("#show_tts").append('<a class="btn btn-niv3 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
+				value.replace("<niv3>", "").replace("</niv3>", "") + "'" + ')">' + value.replace("<niv3>", "").replace("</niv3>", "") + '</a>');
+			} else {
+				$("#show_tts").append('<a class="btn btn-niv1 btn-large" href="#" role="button" onclick="PlaySound(' + "'" + 
+				value + "'" + ')">' + value + '</a>');
+			}
 		}
 	});
 };
+
+function remove_accent(value){
+    var accent = [
+        /[\300-\306]/g, /[\340-\346]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+    ];
+    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+     
+    var str = value;
+    for(var i = 0; i < accent.length; i++){
+        str = str.replace(accent[i], noaccent[i]);
+    }
+     
+    return str;
+}
